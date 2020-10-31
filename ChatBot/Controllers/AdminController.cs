69b9ce63417866d40a4e47d8d486eb8941f6a606
Haythem Bahri <haythem.bahri8@hotmail.com>
2020-10-31 17:54:41 +0100

    initial commit

---
+++ ChatBot/Controllers/AdminController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ChatBot.Models;
+
+namespace ChatBot.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ChatBotEntities db = new ChatBotEntities();
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(db.UserTables.ToList());
+        }
+        public ActionResult Verify()
+        {
+            return View();
+        }
+        // GET: Admin/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTable userTable = db.UserTables.Find(id);
+            if (userTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTable);
+        }
+
+        // GET: Admin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserID,UserName,Password,IsAdmin")] UserTable userTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserTables.Add(userTable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userTable);
+        }
+
+        // GET: Admin/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTable userTable = db.UserTables.Find(id);
+            if (userTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTable);
+        }
+
+        // POST: Admin/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserID,UserName,Password,IsAdmin")] UserTable userTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userTable);
+        }
+
+        // GET: Admin/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTable userTable = db.UserTables.Find(id);
+            if (userTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTable);
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserTable userTable = db.UserTables.Find(id);
+            db.UserTables.Remove(userTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ChatBot/Controllers/UserController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web.Security;
+using System.Web.Mvc;
+using ChatBot.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ChatBot.Controllers
+{
+    public class UserController : Controller
+    {
+        public SqlConnectionStringBuilder strr = new SqlConnectionStringBuilder();
+
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            UserTable userModel = new UserTable();
+
+            return View(userModel);
+        }
+        [HttpPost]
+        public ActionResult SignUp(UserTable user)
+        {
+            int userId = user.UserID;
+            user.IsAdmin = false;
+            using (ChatBotEntities db = new ChatBotEntities())
+            {
+                if (db.UserTables.Any(x => x.UserName == user.UserName))
+                {
+                    ViewBag.Duplicate = "User is duplicated";
+                    return View("SignUp", user);
+                }
+                db.UserTables.Add(user);
+                db.SaveChanges();
+
+            }
+            ModelState.Clear();
+            ViewBag.messageSuccess = "User registred successfully";
+            return View("SignUp", new UserTable { UserID = userId++ });
+        }
+        [HttpGet]
+        public ActionResult LogIn()
+        {
+            ViewBag.logedIn = false;
+            
+            return View("LogIn");
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(UserTable user)
+        {
+            //ViewBag.logedInError = false;
+            using (ChatBotEntities db = new ChatBotEntities())
+            {
+                if (db.UserTables.Any(x => x.UserName == user.UserName && x.Password == user.Password))
+                {
+                    ViewBag.logedIn = true;
+                    return View("~/Views/Home/Index.cshtml");
+                    //return RedirectToAction("index", "Home");
+                }
+                else
+                {
+                    ViewBag.logedInError = true;
+                    return View("LogIn");
+                }
+            }
+            
+        }
+
+        [HttpGet]
+        public ActionResult LogOut()
+        {
+            ViewBag.logedIn = null;
+            ViewBag.logedInError = null;
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("index","Home");
+            
+            //return View("~/Views/Home/Index.cshtml");
+        }
+    }
+}
